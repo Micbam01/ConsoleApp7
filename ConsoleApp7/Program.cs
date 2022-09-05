c89d5c260f781bb28f470f82cefe6b3e6476050a
HP <HP@DESKTOP-ECOEFKE>
2022-09-04 23:54:00 -0700

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Total = 0, interestRate, years, annualCompound, Amount;
+            Console.WriteLine("Enter the Initial Amount:");
+            Amount = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the Rate of Interest:");
+            interestRate = Convert.ToDouble(Console.ReadLine()) / 100;
+            years = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Number of Times the Interest will be Compounded:");
+            annualCompound = Convert.ToDouble(Console.ReadLine());
+            for (int i = 0; i < years; i++)
+            {
+                Total = Amount * Math.Pow((1 + interestRate / annualCompound)(annualCompound * Total)); ;
+                Console.WriteLine("Your Total for Year{0}" + "is {1:F0} .\n", Total, i);
+
+            }
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
